981192c521c9f28560b9b745fb7d696690ecc1cd
Aleksey Yablokov <ya_al@bk.ru>
2024-06-06 12:38:22 +0700

    Remove "pydantic" dependency

--- note_size/size_formatter.py
+++ note_size/size_formatter.py
@@ -1,12 +1,20 @@
-from pydantic import ByteSize
-
-
 class SizeFormatter:
 
     @staticmethod
     def bytes_to_human_str(bytes_size: int) -> str:
-        byte_size: ByteSize = ByteSize(bytes_size)
-        return byte_size.human_readable(True)
+        divisor = 1024
+        units = 'B', 'KB', 'MB'
+        final_unit = 'GB'
+        num = float(bytes_size)
+        for unit in units:
+            if abs(num) < divisor:
+                if unit == 'B':
+                    return f'{num:0.0f}{unit}'
+                else:
+                    return f'{num:0.1f}{unit}'
+            num /= divisor
+
+        return f'{num:0.1f}{final_unit}'
 
     @staticmethod
     def file_sizes_to_human_strings(file_sizes: dict[str, int]) -> list[str]:
--- tests/test_size_formatter.py
+++ tests/test_size_formatter.py
@@ -12,7 +12,12 @@ class SizeFormatterTestCase(unittest.TestCase):
         self.col: Collection = Collection(tempfile.mkstemp(suffix=".anki2")[1])
 
     def test_bytes_to_human_str(self):
-        self.assertEqual("1.6MB", SizeFormatter.bytes_to_human_str(1_600_456))
+        self.assertEqual("0B", SizeFormatter.bytes_to_human_str(0))
+        self.assertEqual("456B", SizeFormatter.bytes_to_human_str(456))
+        self.assertEqual("1.4KB", SizeFormatter.bytes_to_human_str(1_456))
+        self.assertEqual("1.5MB", SizeFormatter.bytes_to_human_str(1_600_456))
+        self.assertEqual("1.7GB", SizeFormatter.bytes_to_human_str(1_784_600_456))
+        self.assertEqual("1626.8GB", SizeFormatter.bytes_to_human_str(1_746_784_600_456))
 
     def test_file_sizes_to_human_str(self):
         file_sizes: dict[str, int] = {'picture.jpg': 7, 'sound.mp3': 5, 'animation.gif': 9}
