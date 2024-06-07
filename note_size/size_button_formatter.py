a182f1ce1ddc4361c78e02bebb660775633cce61
Aleksey Yablokov <ya_al@bk.ru>
2024-06-07 07:13:03 +0700

    Use bs4 for HTML formatting

--- note_size/size_button_formatter.py
+++ note_size/size_button_formatter.py
@@ -1,6 +1,7 @@
 import logging
-import textwrap
 from logging import Logger
+
+from bs4 import BeautifulSoup, Tag
 
 from .size_calculator import SizeCalculator
 from .size_formatter import SizeFormatter
@@ -17,17 +18,33 @@ class SizeButtonFormatter:
         total_files_size: str = SizeFormatter.bytes_to_human_str(SizeCalculator.total_file_size(note))
         file_sizes: dict[str, int] = SizeCalculator.sort_by_size_desc(SizeCalculator.file_sizes(note))
         files_sizes_str: list[str] = SizeFormatter.file_sizes_to_human_strings(file_sizes)
-        files_str: str = '</li><li style="white-space:nowrap">'.join(files_sizes_str) \
-            if len(files_sizes_str) > 0 else "<no-files>"
-        detailed: str = textwrap.dedent(f"""
-                            <h3>Total note size: {total_size}</h3>
-                            <ul>
-                                <li>Texts size: {total_texts_size}</li>
-                                <li>Files size: {total_files_size}</li>
-                                <li>Files (big to small):</li>
-                                    <ol>
-                                        <li style="white-space:nowrap">{files_str}</li>
-                                    </ol>
-                            </ul>
-                            """)
-        return detailed
+
+        soup: BeautifulSoup = BeautifulSoup()
+
+        total_note_size_h3: Tag = soup.new_tag('h3')
+        total_note_size_h3.string = f"Total note size: {total_size}"
+        text_size_li: Tag = soup.new_tag('li')
+        text_size_li.string = f"Texts size: {total_texts_size}"
+        file_size_li: Tag = soup.new_tag('li')
+        file_size_li.string = f"Files size: {total_files_size}"
+        files_li: Tag = soup.new_tag('li')
+        files_li.string = "Files (big to small):"
+
+        files_ol: Tag = soup.new_tag('ol')
+        for files_str in files_sizes_str:
+            li: Tag = soup.new_tag('li', attrs={"style": "white-space:nowrap"})
+            li.string = files_str
+            files_ol.append(li)
+
+        ul: Tag = soup.new_tag('ul')
+        ul.append(text_size_li)
+        ul.append(file_size_li)
+        ul.append(files_li)
+
+        soup.append(total_note_size_h3)
+        soup.append(text_size_li)
+        soup.append(file_size_li)
+        soup.append(files_li)
+        soup.append(files_ol)
+
+        return str(soup.prettify())
--- tests/test_size_button_formatter.py
+++ tests/test_size_button_formatter.py
@@ -1,8 +1,9 @@
 import tempfile
-import textwrap
 import unittest
 
 from anki.collection import Collection
+from anki.notes import Note
+from bs4 import BeautifulSoup
 
 from note_size.size_button_formatter import SizeButtonFormatter
 from tests.data import TestData
@@ -13,21 +14,23 @@ class SizeButtonFormatterTestCase(unittest.TestCase):
     def setUp(self):
         self.col: Collection = Collection(tempfile.mkstemp(suffix=".anki2")[1])
         self.td: TestData = TestData()
-        self.note = self.td.create_note(self.col)
+        self.note: Note = self.td.create_note(self.col)
 
     def test_total_text_size(self):
+        exp_html: str = """
+                    <h3>Total note size: 129B</h3>
+                    <li>Texts size: 108B</li>
+                    <li>Files size: 21B</li>
+                    <li>Files (big to small):</li>
+                    <ol>
+                        <li style="white-space:nowrap">animation.gif: 9B</li>
+                        <li style="white-space:nowrap">picture.jpg: 7B</li>
+                        <li style="white-space:nowrap">sound.mp3: 5B</li>
+                    </ol>
+                    """
+        soup: BeautifulSoup = BeautifulSoup(exp_html, 'html.parser')
         act_text: str = SizeButtonFormatter.format_note_detailed_text(self.note)
-        exp_text: str = textwrap.dedent("""
-        <h3>Total note size: 129B</h3>
-        <ul>
-            <li>Texts size: 108B</li>
-            <li>Files size: 21B</li>
-            <li>Files (big to small):</li>
-                <ol>
-                    <li style="white-space:nowrap">animation.gif: 9B</li><li style="white-space:nowrap">picture.jpg: 7B</li><li style="white-space:nowrap">sound.mp3: 5B</li>
-                </ol>
-        </ul>
-        """)
+        exp_text: str = str(soup.prettify())
         self.assertEqual(exp_text, act_text)
 
     def tearDown(self):
