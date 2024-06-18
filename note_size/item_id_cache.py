79adc10b7febeee3511c4045dd45c65bcf443355
Aleksey Yablokov <ya_al@bk.ru>
2024-06-18 13:59:04 +0700

    Add texts and files sizes in ItemIdCache

--- note_size/item_id_cache.py
+++ note_size/item_id_cache.py
@@ -19,8 +19,12 @@ class ItemIdCache:
         self.col: Collection = col
         self.size_calculator: SizeCalculator = size_calculator
         self.id_cache: dict[CardId, NoteId] = {}
-        self.size_bytes_cache: dict[NoteId, SizeBytes] = {}
-        self.size_str_cache: dict[NoteId, SizeStr] = {}
+        self.size_total_bytes_cache: dict[NoteId, SizeBytes] = {}
+        self.size_total_str_cache: dict[NoteId, SizeStr] = {}
+        self.size_texts_bytes_cache: dict[NoteId, SizeBytes] = {}
+        self.size_texts_str_cache: dict[NoteId, SizeStr] = {}
+        self.size_files_bytes_cache: dict[NoteId, SizeBytes] = {}
+        self.size_files_str_cache: dict[NoteId, SizeStr] = {}
         self.lock: RLock = RLock()
 
     def warm_up_cache(self):
@@ -30,6 +34,8 @@ class ItemIdCache:
             for note_id in all_note_ids:
                 self.get_note_size(note_id, use_cache=True)
                 self.get_note_size_str(note_id, use_cache=True)
+                self.get_note_size_texts_str(note_id, use_cache=True)
+                self.get_note_size_files_str(note_id, use_cache=True)
             all_card_ids = self.col.find_cards("deck:*")
             for card_id in all_card_ids:
                 self.get_note_id_by_card_id(card_id)
@@ -45,18 +51,54 @@ class ItemIdCache:
 
     def get_note_size(self, note_id: NoteId, use_cache: bool) -> SizeBytes:
         with self.lock:
-            if use_cache and note_id in self.size_bytes_cache:
-                return self.size_bytes_cache[note_id]
+            if use_cache and note_id in self.size_total_bytes_cache:
+                return self.size_total_bytes_cache[note_id]
+            else:
+                self.size_total_bytes_cache[note_id] = SizeBytes(self.get_note_texts_size(note_id, use_cache) +
+                                                                 self.get_note_files_size(note_id, use_cache))
+                return self.size_total_bytes_cache[note_id]
+
+    def get_note_texts_size(self, note_id: NoteId, use_cache: bool) -> SizeBytes:
+        with self.lock:
+            if use_cache and note_id in self.size_texts_bytes_cache:
+                return self.size_texts_bytes_cache[note_id]
             else:
                 note: Note = self.col.get_note(note_id)
-                self.size_bytes_cache[note_id] = self.size_calculator.calculate_note_size(note)
-                return self.size_bytes_cache[note_id]
+                self.size_texts_bytes_cache[note_id] = self.size_calculator.calculate_texts_size(note)
+                return self.size_texts_bytes_cache[note_id]
+
+    def get_note_files_size(self, note_id: NoteId, use_cache: bool) -> SizeBytes:
+        with self.lock:
+            if use_cache and note_id in self.size_files_bytes_cache:
+                return self.size_files_bytes_cache[note_id]
+            else:
+                note: Note = self.col.get_note(note_id)
+                self.size_files_bytes_cache[note_id] = self.size_calculator.calculate_files_size(note)
+                return self.size_files_bytes_cache[note_id]
 
     def get_note_size_str(self, note_id: NoteId, use_cache: bool) -> SizeStr:
         with self.lock:
-            if use_cache and note_id in self.size_str_cache:
-                return self.size_str_cache[note_id]
+            if use_cache and note_id in self.size_total_str_cache:
+                return self.size_total_str_cache[note_id]
             else:
                 size: SizeBytes = self.get_note_size(note_id, use_cache)
-                self.size_str_cache[note_id] = SizeFormatter.bytes_to_str(size)
-                return self.size_str_cache[note_id]
+                self.size_total_str_cache[note_id] = SizeFormatter.bytes_to_str(size)
+                return self.size_total_str_cache[note_id]
+
+    def get_note_size_texts_str(self, note_id: NoteId, use_cache: bool) -> SizeStr:
+        with self.lock:
+            if use_cache and note_id in self.size_texts_str_cache:
+                return self.size_texts_str_cache[note_id]
+            else:
+                size: SizeBytes = self.get_note_texts_size(note_id, use_cache)
+                self.size_texts_str_cache[note_id] = SizeFormatter.bytes_to_str(size)
+                return self.size_texts_str_cache[note_id]
+
+    def get_note_size_files_str(self, note_id: NoteId, use_cache: bool) -> SizeStr:
+        with self.lock:
+            if use_cache and note_id in self.size_files_str_cache:
+                return self.size_files_str_cache[note_id]
+            else:
+                size: SizeBytes = self.get_note_files_size(note_id, use_cache)
+                self.size_files_str_cache[note_id] = SizeFormatter.bytes_to_str(size)
+                return self.size_files_str_cache[note_id]
