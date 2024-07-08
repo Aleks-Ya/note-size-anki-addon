7da55d402b6210a101c2b41c566b2f0c50b8f541
Aleksey Yablokov <ya_al@bk.ru>
2024-07-08 20:31:08 +0800

    Add titles for sizes in DeckBrowser

--- note_size/deck_browser/deck_browser_hooks.py
+++ note_size/deck_browser/deck_browser_hooks.py
@@ -25,6 +25,8 @@ class DeckBrowserHooks:
         self.__media_cache: MediaCache = media_cache
         self.__item_id_cache: ItemIdCache = item_id_cache
         self.__hook_deck_browser_will_render_content: Callable[[Any, Any], None] = self.__on_action
+        self.__collection_file_path: Path = Path(mw.pm.profileFolder(), "collection.anki2")
+        self.__media_folder_path: Path = Path(mw.col.media.dir())
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def setup_hooks(self) -> None:
@@ -41,25 +43,28 @@ class DeckBrowserHooks:
         collection_size: SizeStr = SizeFormatter.bytes_to_str(self.__collection_size())
         media_size: SizeStr = SizeFormatter.bytes_to_str(self.__media_size())
         total_size: SizeStr = SizeFormatter.bytes_to_str(self.__total_size())
+        collection_title: str = f'Size of file "{self.__collection_file_path}"'
+        media_title: str = f'Size of folder "{self.__media_folder_path}"'
+        total_title: str = f'Total size of collection file and media folder'
         collection_span: str = (
-            f"<span id='{DeckBrowserHooks.__collection_size_id}' title='Size of note fields without media' "
+            f"<span id='{DeckBrowserHooks.__collection_size_id}' "
             f"style='{DeckBrowserHooks.__code_style}'>{collection_size}</span>")
-        media_span: str = (f"<span id='{DeckBrowserHooks.__media_size_id}' title='Size of media files without fields' "
+        media_span: str = (f"<span id='{DeckBrowserHooks.__media_size_id}' "
                            f"style='{DeckBrowserHooks.__code_style}'>{media_size}</span>")
-        total_span: str = (f"<span id='{DeckBrowserHooks.__total_size_id}' title='Size of note fields with media' "
+        total_span: str = (f"<span id='{DeckBrowserHooks.__total_size_id}' "
                            f"style='{DeckBrowserHooks.__code_style}'>{total_size}</span>")
-        content.stats += (f"<div>Collection:&nbsp;{collection_span}&nbsp;&nbsp;&nbsp;"
-                          f"Media:&nbsp;{media_span}&nbsp;&nbsp;&nbsp;"
-                          f"Total:&nbsp;{total_span}&nbsp;&nbsp;&nbsp;</div>")
+        content.stats += (f"<div>"
+                          f"<span title='{collection_title}'>Collection:&nbsp;{collection_span}&nbsp;&nbsp;&nbsp;</span>"
+                          f"<span title='{media_title}'>Media:&nbsp;{media_span}&nbsp;&nbsp;&nbsp;</span>"
+                          f"<span title='{total_title}'>Total:&nbsp;{total_span}</span>"
+                          f"</div>")
         log.info(f"DeckBrowserContent stats (edited): {content.stats}\n\n")
 
     def __media_size(self) -> SizeBytes:
         return self.__media_cache.get_total_files_size()
 
-    @staticmethod
-    def __collection_size() -> SizeBytes:
-        collection_file_path: Path = Path(mw.pm.profileFolder(), "collection.anki2")
-        return SizeBytes(collection_file_path.stat().st_size)
+    def __collection_size(self) -> SizeBytes:
+        return SizeBytes(self.__collection_file_path.stat().st_size)
 
     def __total_size(self) -> SizeBytes:
         return SizeBytes(self.__collection_size() + self.__media_size())
