d8e80b5c9791110555a1c4af03c6a2a7d6c732a9
Aleksey Yablokov <ya_al@bk.ru>
2024-07-08 20:05:12 +0800

    Remove Refresh button from DeckBrowser

--- note_size/deck_browser/deck_browser_hooks.py
+++ note_size/deck_browser/deck_browser_hooks.py
@@ -1,4 +1,3 @@
-import datetime
 import logging
 from logging import Logger
 from pathlib import Path
@@ -26,23 +25,19 @@ class DeckBrowserHooks:
         self.__media_cache: MediaCache = media_cache
         self.__item_id_cache: ItemIdCache = item_id_cache
         self.__hook_deck_browser_will_render_content: Callable[[Any, Any], None] = self.__on_action
-        self.__hook_webview_did_receive_js_message = self.__on_event
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def setup_hooks(self) -> None:
         gui_hooks.deck_browser_will_render_content.append(self.__hook_deck_browser_will_render_content)
-        gui_hooks.webview_did_receive_js_message.append(self.__hook_webview_did_receive_js_message)
         log.info(f"{self.__class__.__name__} are set")
 
     def remove_hooks(self) -> None:
         gui_hooks.deck_browser_will_render_content.remove(self.__hook_deck_browser_will_render_content)
-        gui_hooks.webview_did_receive_js_message.remove(self.__hook_webview_did_receive_js_message)
         log.info(f"{self.__class__.__name__} are removed")
 
     # noinspection PyUnresolvedReferences
     def __on_action(self, _: 'aqt.deckbrowser.DeckBrowser',
                     content: 'aqt.deckbrowser.DeckBrowserContent') -> None:
-        on_click_js: str = f"pycmd('{DeckBrowserHooks.__my_python_action}')"
         collection_size: SizeStr = SizeFormatter.bytes_to_str(self.__collection_size())
         media_size: SizeStr = SizeFormatter.bytes_to_str(self.__media_size())
         total_size: SizeStr = SizeFormatter.bytes_to_str(self.__total_size())
@@ -53,13 +48,9 @@ class DeckBrowserHooks:
                            f"style='{DeckBrowserHooks.__code_style}'>{media_size}</span>")
         total_span: str = (f"<span id='{DeckBrowserHooks.__total_size_id}' title='Size of note fields with media' "
                            f"style='{DeckBrowserHooks.__code_style}'>{total_size}</span>")
-        button: str = (f"<button id='{DeckBrowserHooks.__button_id}' title='Update' "
-                       f"""style="padding: 2px 5px;" onclick="{on_click_js}">⟳</button>""")
-        content.stats += (f"<div style='align-items: center; display: flex; justify-content: center;'>"
-                          f"Collection:&nbsp;{collection_span}&nbsp;&nbsp;&nbsp;"
+        content.stats += (f"<div>Collection:&nbsp;{collection_span}&nbsp;&nbsp;&nbsp;"
                           f"Media:&nbsp;{media_span}&nbsp;&nbsp;&nbsp;"
-                          f"Total:&nbsp;{total_span}&nbsp;&nbsp;&nbsp;"
-                          f"{button}</div>")
+                          f"Total:&nbsp;{total_span}&nbsp;&nbsp;&nbsp;</div>")
         log.info(f"DeckBrowserContent stats (edited): {content.stats}\n\n")
 
     def __media_size(self) -> SizeBytes:
@@ -72,18 +63,3 @@ class DeckBrowserHooks:
 
     def __total_size(self) -> SizeBytes:
         return SizeBytes(self.__collection_size() + self.__media_size())
-
-    def __on_event(self, handled: tuple[bool, Any], message: str, _: Any) -> tuple[bool, Any]:
-        if message == DeckBrowserHooks.__my_python_action:
-            collection_size: SizeStr = SizeFormatter.bytes_to_str(self.__collection_size())
-            media_size: SizeStr = SizeFormatter.bytes_to_str(self.__media_size())
-            total_size: SizeStr = SizeFormatter.bytes_to_str(self.__total_size())
-            button_title: str = f"Updated {datetime.datetime.now().time().replace(microsecond=0)}"
-            on_click_js: str = (
-                f"document.getElementById('{DeckBrowserHooks.__collection_size_id}').textContent='{collection_size}';"
-                f"document.getElementById('{DeckBrowserHooks.__media_size_id}').textContent='{media_size}';"
-                f"document.getElementById('{DeckBrowserHooks.__total_size_id}').textContent='{total_size}';"
-                f"document.getElementById('{DeckBrowserHooks.__button_id}').title='{button_title}';")
-            mw.web.eval(on_click_js)
-            return True, None
-        return handled
