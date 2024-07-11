13ab1d3aa6085604cd13dc25051d1ee3ec60144a
Aleksey Yablokov <ya_al@bk.ru>
2024-07-11 14:46:51 +0800

    Assert cell alignment in TestColumnHooks

--- tests/column/test_column_hooks.py
+++ tests/column/test_column_hooks.py
@@ -59,6 +59,7 @@ class TestColumnHooks(unittest.TestCase):
                 sorting_cards=BrowserColumns.SORTING_DESCENDING,
                 sorting_notes=BrowserColumns.SORTING_DESCENDING,
                 uses_cell_font=False,
+                alignment=BrowserColumns.ALIGNMENT_START,
                 cards_mode_tooltip="Note size (texts and files are included)",
                 notes_mode_tooltip="Note size (texts and files are included)"
             ),
@@ -69,6 +70,7 @@ class TestColumnHooks(unittest.TestCase):
                 sorting_cards=BrowserColumns.SORTING_DESCENDING,
                 sorting_notes=BrowserColumns.SORTING_DESCENDING,
                 uses_cell_font=False,
+                alignment=BrowserColumns.ALIGNMENT_START,
                 cards_mode_tooltip="Note size (texts only, files are not included)",
                 notes_mode_tooltip="Note size (texts only, files are not included)"
             ),
@@ -79,6 +81,7 @@ class TestColumnHooks(unittest.TestCase):
                 sorting_cards=BrowserColumns.SORTING_DESCENDING,
                 sorting_notes=BrowserColumns.SORTING_DESCENDING,
                 uses_cell_font=False,
+                alignment=BrowserColumns.ALIGNMENT_START,
                 cards_mode_tooltip="Note size (files only, texts are not included)",
                 notes_mode_tooltip="Note size (files only, texts are not included)"
             )
