6a72b9abbf8ea5d734f142191264b6457cca62c8
Aleksey Yablokov <alex_ya@mailbox.org>
2024-08-04 09:07:58 +0800

    Read cache file in QueryOp

--- note_size/cache/cache_updater.py
+++ note_size/cache/cache_updater.py
@@ -26,13 +26,17 @@ class CacheUpdater:
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def initialize_caches(self):
-        self.__item_id_cache.read_caches_from_file()
+        self.__read_cache_from_file()
         self.__warmup_caches(mw)
+
+    def __read_cache_from_file(self):
+        op: QueryOp = QueryOp(parent=mw, op=self.__read_cache_file_background_op,
+                              success=self.__read_cache_file_success)
+        op.with_progress().run_in_background()
 
     def __warmup_caches(self, parent: QWidget):
         log.info("Warmup caches")
-        self.__media_cache.invalidate_cache()
-        op: QueryOp = QueryOp(parent=parent, op=self.__background_op, success=self.__success)
+        op: QueryOp = QueryOp(parent=parent, op=self.__warmup_background_op, success=self.__warmup_success)
         op.with_progress().run_in_background()
 
     def refresh_caches(self, parent: QWidget):
@@ -40,10 +44,10 @@ class CacheUpdater:
         self.__item_id_cache.delete_cache_file()
         self.__media_cache.invalidate_cache()
         self.__item_id_cache.invalidate_caches()
-        op: QueryOp = QueryOp(parent=parent, op=self.__background_op, success=self.__success)
+        op: QueryOp = QueryOp(parent=parent, op=self.__warmup_background_op, success=self.__warmup_success)
         op.with_progress().run_in_background()
 
-    def __background_op(self, col: Collection) -> int:
+    def __warmup_background_op(self, col: Collection) -> int:
         if not self.__config.get_cache_warmup_enabled():
             log.info("Cache warmup is disabled")
             return 0
@@ -67,10 +71,21 @@ class CacheUpdater:
         log.info(f"Cache warmup finished: notes={len(all_note_ids)}, cards={len(all_card_ids)}, "
                  f"duration_sec={duration_sec}, {self.__item_id_cache.get_size()}")
 
-    def __update_progress(self, label: str, value: int, max_value: int):
+    def __read_cache_file_background_op(self, _: Collection) -> int:
+        mw.progress.update(label="Reading note size cache from file...")
+        self.__item_id_cache.read_caches_from_file()
+        return 0
+
+    @staticmethod
+    def __update_progress(label: str, value: int, max_value: int):
         if value % 1000 == 0:
             mw.taskman.run_on_main(
                 lambda: mw.progress.update(label=f"{label}: {value} of {max_value}", value=value, max=max_value))
 
-    def __success(self, count: int) -> None:
+    @staticmethod
+    def __warmup_success(count: int) -> None:
         log.info(f"Cache initialization finished: {count}")
+
+    @staticmethod
+    def __read_cache_file_success(_: int) -> None:
+        log.info(f"Reading cache file finished")
