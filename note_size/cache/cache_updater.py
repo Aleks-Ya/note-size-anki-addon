e1ad06e98e78ff4833ff2c771f6fb57d801f2060
Aleksey Yablokov <alex_ya@mailbox.org>
2024-08-04 10:15:25 +0800

    Show message after cache refresh

--- note_size/cache/cache_updater.py
+++ note_size/cache/cache_updater.py
@@ -8,6 +8,7 @@ from anki.notes import NoteId
 from aqt import mw
 from aqt.operations import QueryOp
 from aqt.qt import QWidget
+from aqt.utils import showInfo
 
 from .item_id_cache import ItemIdCache
 from .media_cache import MediaCache
@@ -18,6 +19,7 @@ log: Logger = logging.getLogger(__name__)
 
 
 class CacheUpdater:
+    __op_title: str = '"Note Size" addon'
 
     def __init__(self, media_cache: MediaCache, item_id_cache: ItemIdCache, config: Config):
         self.__media_cache: MediaCache = media_cache
@@ -49,7 +51,7 @@ class CacheUpdater:
         self.__item_id_cache.delete_cache_file()
         self.__media_cache.invalidate_cache()
         self.__item_id_cache.invalidate_caches()
-        op: QueryOp = QueryOp(parent=parent, op=self.__warmup_background_op, success=self.__warmup_success)
+        op: QueryOp = QueryOp(parent=parent, op=self.__warmup_background_op, success=self.__refresh_success)
         op.with_progress().run_in_background()
 
     def __warmup_background_op(self, col: Collection) -> int:
@@ -60,28 +62,33 @@ class CacheUpdater:
         start_time: datetime = datetime.now()
 
         all_note_ids: Sequence[NoteId] = col.find_notes("deck:*")
+        note_number: int = len(all_note_ids)
         for i, note_id in enumerate(all_note_ids):
             for size_type in size_types:
-                self.__update_progress("Caching note sizes", i, len(all_note_ids))
+                self.__update_progress("Caching note sizes", i, note_number)
                 self.__item_id_cache.get_note_size_bytes(note_id, size_type, use_cache=True)
                 self.__item_id_cache.get_note_size_str(note_id, size_type, use_cache=True)
 
         all_card_ids: Sequence[int] = col.find_cards("deck:*")
+        card_number: int = len(all_card_ids)
         for i, card_id in enumerate(all_card_ids):
-            self.__update_progress("Caching card sizes", i, len(all_card_ids))
+            self.__update_progress("Caching card sizes", i, card_number)
             self.__item_id_cache.get_note_id_by_card_id(card_id)
 
         end_time: datetime = datetime.now()
         duration_sec: int = round((end_time - start_time).total_seconds())
-        log.info(f"Cache warmup finished: notes={len(all_note_ids)}, cards={len(all_card_ids)}, "
+        log.info(f"Cache warmup finished: notes={note_number}, cards={card_number}, "
                  f"duration_sec={duration_sec}, {self.__item_id_cache.get_size()}")
+        return note_number + card_number
 
     def __read_cache_file_background_op(self, _: Collection) -> int:
+        mw.progress.set_title(CacheUpdater.__op_title)
         mw.progress.update(label="Reading note size cache from file...")
         self.__item_id_cache.read_caches_from_file()
         return 0
 
     def __save_cache_file_background_op(self, _: Collection) -> int:
+        mw.progress.set_title(CacheUpdater.__op_title)
         mw.progress.update(label="Saving note size cache from file...")
         self.__item_id_cache.save_caches_to_file()
         return 0
@@ -89,12 +96,21 @@ class CacheUpdater:
     @staticmethod
     def __update_progress(label: str, value: int, max_value: int):
         if value % 1000 == 0:
-            mw.taskman.run_on_main(
-                lambda: mw.progress.update(label=f"{label}: {value} of {max_value}", value=value, max=max_value))
+            mw.taskman.run_on_main(lambda: CacheUpdater.__update_mw_progress(label, value, max_value))
+
+    @staticmethod
+    def __update_mw_progress(label: str, value: int, max_value: int):
+        mw.progress.set_title(CacheUpdater.__op_title)
+        mw.progress.update(label=f"{label}: {value} of {max_value}", value=value, max=max_value)
 
     @staticmethod
     def __warmup_success(count: int) -> None:
-        log.info(f"Cache initialization finished: {count}")
+        log.info(f"Cache warmup finished: {count}")
+
+    @staticmethod
+    def __refresh_success(count: int) -> None:
+        log.info(f"Cache refresh finished: {count}")
+        showInfo(title=CacheUpdater.__op_title, text=f"Cache was refreshed ({count} notes and cards)")
 
     @staticmethod
     def __read_cache_file_success(_: int) -> None:
