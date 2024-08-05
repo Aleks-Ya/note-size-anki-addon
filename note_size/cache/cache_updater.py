08effbe6133771d31008ca80cf6cdc88af33e307
Aleksey Yablokov <alex_ya@mailbox.org>
2024-08-05 09:31:50 +0800

    Do not show progress on cache warmup

--- note_size/cache/cache_updater.py
+++ note_size/cache/cache_updater.py
@@ -67,19 +67,23 @@ class _SaveCacheFromFileOp:
 class _WarmupCacheOp:
     __progress_dialog_title: str = '"Note Size" addon'
 
-    def __init__(self, media_cache: MediaCache, item_id_cache: ItemIdCache, config: Config, parent: QWidget):
+    def __init__(self, media_cache: MediaCache, item_id_cache: ItemIdCache, config: Config, parent: QWidget,
+                 with_progress: bool):
         self.__media_cache: MediaCache = media_cache
         self.__item_id_cache: ItemIdCache = item_id_cache
         self.__config: Config = config
         self.__parent: QWidget = parent
+        self.__with_progress: bool = with_progress
         self.__on_success: Callable[[int], None] = self.__on_warmup_success
         log.debug(f"{self.__class__.__name__} was instantiated")
 
     def warmup_caches_in_background(self):
         if self.__config.get_cache_warmup_enabled():
             log.info("Warmup caches")
-            QueryOp(parent=self.__parent, op=self.__background_op,
-                    success=self.__on_success).with_progress().run_in_background()
+            op = QueryOp(parent=self.__parent, op=self.__background_op, success=self.__on_success)
+            if self.__with_progress:
+                op = op.with_progress()
+            op.run_in_background()
         else:
             log.info("Cache warmup is disabled")
 
@@ -88,7 +92,8 @@ class _WarmupCacheOp:
         return self
 
     def __background_op(self, col: Collection) -> int:
-        mw.progress.set_title(self.__progress_dialog_title)
+        if self.__with_progress:
+            mw.progress.set_title(self.__progress_dialog_title)
         log.info(f"Cache warmup started: {self.__item_id_cache.get_size()}")
         start_time: datetime = datetime.now()
 
@@ -112,11 +117,11 @@ class _WarmupCacheOp:
                  f"duration_sec={duration_sec}, {self.__item_id_cache.get_size()}")
         return note_number + card_number
 
-    @staticmethod
-    def __update_progress(label: str, value: int, max_value: int):
-        if value % 1000 == 0:
-            mw.taskman.run_on_main(
-                lambda: mw.progress.update(label=f"{label}: {value} of {max_value}", value=value, max=max_value))
+    def __update_progress(self, label: str, value: int, max_value: int):
+        if self.__with_progress:
+            if value % 1000 == 0:
+                mw.taskman.run_on_main(
+                    lambda: mw.progress.update(label=f"{label}: {value} of {max_value}", value=value, max=max_value))
 
     @staticmethod
     def __on_warmup_success(count: int) -> None:
@@ -136,7 +141,8 @@ class CacheUpdater:
 
     def initialize_caches(self):
         _ReadCacheFromFileOp(self.__item_id_cache, self.__config).read_cache_from_file_in_background()
-        _WarmupCacheOp(self.__media_cache, self.__item_id_cache, self.__config, mw).warmup_caches_in_background()
+        _WarmupCacheOp(self.__media_cache, self.__item_id_cache, self.__config, mw,
+                       with_progress=False).warmup_caches_in_background()
 
     def refresh_caches(self, parent: QWidget):
         log.info("Refresh caches")
@@ -144,7 +150,7 @@ class CacheUpdater:
         self.__media_cache.invalidate_cache()
         self.__item_id_cache.invalidate_caches()
         _WarmupCacheOp(self.__media_cache, self.__item_id_cache, self.__config,
-                       parent).with_on_refresh_success().warmup_caches_in_background()
+                       parent, with_progress=True).with_on_refresh_success().warmup_caches_in_background()
 
     def save_cache_to_file(self):
         _SaveCacheFromFileOp(self.__item_id_cache, self.__config).save_cache_to_file_in_background()
