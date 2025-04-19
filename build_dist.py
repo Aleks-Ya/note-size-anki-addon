c5ad8d3113345c2e78a20beadc56b7ff85440094
Aleksey Yablokov <alex_ya@mailbox.org>
2025-04-19 18:30:04 +0700

    Upgrade to Anki 25.2.1

--- build_dist.py
+++ build_dist.py
@@ -76,7 +76,7 @@ class DistributionBuilder:
             "package": "1188705668",
             "author": "Alexey Yablokov",
             "min_point_version": 240401,
-            "max_point_version": 250200,
+            "max_point_version": 250201,
             "human_version": version,
             "homepage": "https://ankiweb.net/shared/info/1188705668",
             "mod": commit_epoch_sec
