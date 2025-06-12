efbead23f5e5ce2537625a7525f6310bd0a8a07b
Aleksey Yablokov <alex_ya@mailbox.org>
2025-06-12 22:55:34 +0700

    Update anki-qt6-current.txt to 5.2.7

--- tests/ui/editor/button/editor_button_creator_test.py
+++ tests/ui/editor/button/editor_button_creator_test.py
@@ -5,15 +5,16 @@ from note_size.ui.editor.button.editor_button_creator import EditorButtonCreator
 
 
 @pytest.mark.skip_for_beta
-def test_create_size_button(editor_button_creator: EditorButtonCreator, editor_edit_mode: Editor,
-                            editor_add_mode: Editor):
+@pytest.mark.skip_for_earliest
+def test_create_size_button_current(editor_button_creator: EditorButtonCreator, editor_edit_mode: Editor,
+                                    editor_add_mode: Editor):
     exp: str = """<button tabindex=-1
                         id=size_button
-                        class="linkb perm"
+                        class="anki-addon-button linkb perm"
                         type="button"
                         title="Note size. Click for details"
-                        onclick="pycmd('size_button_cmd');return false;"
-                        onmousedown="window.event.preventDefault();"
+                        data-cantoggle="0"
+                        data-command="size_button_cmd"
                 >
                     
                     0 B
@@ -33,6 +34,25 @@ def test_create_size_button_beta(editor_button_creator: EditorButtonCreator, edi
                         title="Note size. Click for details"
                         data-cantoggle="0"
                         data-command="size_button_cmd"
+                >
+                    
+                    0 B
+                </button>"""
+    assert editor_button_creator.create_size_button(editor_edit_mode) == exp
+    assert editor_button_creator.create_size_button(editor_add_mode) == exp
+
+
+@pytest.mark.skip_for_beta
+@pytest.mark.skip_for_current
+def test_create_size_button_earliest(editor_button_creator: EditorButtonCreator, editor_edit_mode: Editor,
+                                     editor_add_mode: Editor):
+    exp: str = """<button tabindex=-1
+                        id=size_button
+                        class="linkb perm"
+                        type="button"
+                        title="Note size. Click for details"
+                        onclick="pycmd('size_button_cmd');return false;"
+                        onmousedown="window.event.preventDefault();"
                 >
                     
                     0 B
